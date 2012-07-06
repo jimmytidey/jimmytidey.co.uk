f0ed771d83e67a45729933c07adb9d6b354d5291
jimmytidey <jimmytidey@gmail.com>
2012-07-06 22:34:24 +0100

    automated commit

---
+++ html/article.php
@@ -0,0 +1,19 @@
+<?
+
+
+$article_id = $_GET['article_id']; 
+header("HTTP/1.1 301 Moved Permanently");
+
+if ($article_id == 'Local+by+Social+-++Where+sexy+social+media+and+bashful+local+government+go+to+flirt') { 
+    
+    header("Location: http://jimmytidey.co.uk/blog/royal-society-web-science-event/");
+    
+}
+
+
+
+
+
+
+
+?>
