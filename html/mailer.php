0d7ee9c5b7234ea01a3cfb2e2eaada6a47d1e864
Jimmy Tidey <jimmytidey@gmail.com>
2013-09-03 13:50:19 +0100

    adding quick test script for RSA

---
+++ html/mailer.php
@@ -0,0 +1,11 @@
+<?
+    
+    
+    $to         = 'jimmytidey@gmail.com';
+    $subject    = 'THIS IS AN EMAIL';
+    $message    = 'THIS IS AN EMAIL BODY';
+    
+    mail($to, $subject, $message); 
+     
+
+?>
