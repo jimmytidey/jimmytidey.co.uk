73967e468fd657521e21fc15ceb3f082a789b2f3
Jimmy Tidey <jimmytidey@gmail.com>
2013-09-03 19:06:12 +0100

    automated commit

--- html/mailer.php
+++ html/mailer.php
@@ -4,10 +4,16 @@
     $to         = 'jimmytidey@gmail.com';
     
     
-    $subject    = 'THIS IS AN EMAIL';
+    $subject    = 'SOCIAL MIRROR EMAIL';
     $message    = $_GET['email_address'];
+    $user_address    = $_GET['email_address'];
     
-    mail($to, $subject, $message); 
+    
+    mail($to, $subject, $message);
+    
+    $message = 'This is a place holder for your prescription'
+     
+    mail($user_address, $subject, $message); 
      
 
 ?>
