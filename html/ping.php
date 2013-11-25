dec329a1c61736d0f07559642bf96acdd3841429
Jimmy Tidey <jimmytidey@gmail.com>
2013-11-25 10:23:37 +0000

    new

--- html/ping.php
+++ html/ping.php
@@ -1,7 +1,10 @@
 <? 
 $value = mail("jimmytidey@gmail.com", "ping", "ping"); 
 
+$output = file_get_contents('http://heresayprinter.eu01.aws.af.cm/push')
+
 ?>
 
 <h1> Mail Sent</h1>
-value = <?=$value ?>
+value = <?=$value ?> <br/> 
+value = <?=$output ?>
