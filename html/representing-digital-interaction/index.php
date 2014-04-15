b97811fc449794cb26c6aec3052b951c2a40c833
Jimmy Tidey <jimmytidey@gmail.com>
2014-04-15 16:43:54 +0100

    representing digital interaction

---
+++ html/representing-digital-interaction/index.php
@@ -0,0 +1,60 @@
+
+<?php include('header.php') ?>
+
+<div class='container'>
+    <div class='row'>
+        <div class='col-md-6 col-md-offset-3'>
+            <h1>Making digital social activity visible</h1>
+        </div>
+    </div>
+    
+    <div class='row'>
+        <div class='col-md-6 col-md-offset-3'>
+            
+            <p>I'm doing a <a href='http://thecreativeexchange.org/'>PhD at RCA</a>. I'm investigating new ways of representing social digital activity.</p>
+            
+            <h2>Maximising the value of digital social interactions</h2>
+            
+            <p>My hypothesis is that existing digital social interactions have a value that isn't being fully explored.</p>
+            
+            <p>Services such as Facebook or Twitter only share information between people who know they want to be connected. Is it possible to help communities achieve their goals by reducing barriers to interaction?</p>
+            
+            <p>In the same way that architects plan offices to improve collaboration, or town planners seek to foster community, can design interventions improve shared digital activity?</p>
+            
+            <!-- <p>With the RSA I'll be investigating what NESTA call "below the radar" community activity to discover community assets, for example film clubs, pub quizzes, informal support networks. This community activity will be discovered by ingesting Twitter, Facebook and community websites.</p> -->
+            
+            
+            
+            <h2>Types of interface - a design opportunity</h2>
+            
+            <p>Nearly all digital activity is mediated through a screen, from writing documents to managing your calendar to social networking. Are there better interfaces for specific tasks?</p>
+            
+            <img src='img/three_activities.png' class='center' />
+                        
+            <p>Perhaps lots of digital activities could benefit from bespoke interfaces.  </p>
+            
+            <img src='img/three_interface_three_person.png' class='center' />
+            
+            <p>This project is investigating alternative interfaces that are intentionally public : print, projection, shared screens.</p>
+            
+            <h2>Interfaces that build community</h2>
+            
+            <p>Do people want to access social digital activity through these interfaces? If they do, does it make it help the collaborate? Can they make decisions better together? Do they feel closer?</p>
+            
+            <p>Just as 
+            
+            
+            <h2>Existing prototypes</h2>    
+            
+            
+        </div>
+    </div>
+    
+
+</div>
+
+<?php  include 'footer.php' ?>
+
+    
+
+
