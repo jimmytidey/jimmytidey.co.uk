9d2133ebb333821e76c5789aead05a7603bfb094
Jimmy <jimmy@Jimmy.(none)>
2014-04-21 20:02:21 +0100

    restoring homepage

--- html/index.php
+++ html/index.php
@@ -1,114 +1,216 @@
+<!DOCTYPE html>
 
-<?php include('header.php') ?>
+<html>
+<head>
 
-<div class='container'>
-    <div class='row'>
-        <div id='background' class='col-md-6 col-md-offset-3'>
-            <h1>Making digital social activity visible</h1>
-        </div>
-    </div>
+    <meta charset=utf-8 />
+
+
+    <title>Jimmy Tidey</title>
+    <meta name="google-site-verification" content="zQd8pef_mazi8KBab_uSbTF8BsxNJAE62AUWO2hgUho" />
+    <meta name="description" content="Jimmy Tidey - Product Manager and Creative Technologist" />
     
-    <div class='row'>
-        <div class='navbar col-md-2 col-md-offset-1'>
-            <ul class='nav'>
-                <li><a href='#background'>Background</a></li>
-                <li><a href='#hypothesis'>Research Question</a></li>
-                <li><a href='#analogy'>An Analogy</a></li>
-                <li><a href='#prototyping'>Prototyping</a></li>
-                <li><a href='#work_so_far'>Work so far</a></li>
-                
-            </ul>
+
+    <link rel="stylesheet" type="text/css" media="screen" href="style/main.css" />
+    <link rel="stylesheet" type="text/css" href="script/jquery.fancybox.css?v=2.1.2" media="screen" />
+        
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+    <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+    
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.5.min.js"></script>
+    <script type="text/javascript" src="http://use.typekit.com/urd8lfs.js"></script>
+    <script type='text/javascript' src='script/script.js' ></script> 
+
+    <script type="text/javascript" src="script/jquery.mousewheel-3.0.6.pack.js"></script>
+    <script type="text/javascript" src="script/jquery.fancybox.pack.js"></script>
+
+    
+    <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+ 
+</head> 
+
+<body>
+    <div id='container'>
+
+        <h1 class='tk-museo' id='name'>Jimmy Tidey</h1>
+        <p id='tag'><strong>Deep thinking about the web</strong> 
+        
+        <div id='contacts' >
+            <p><a target='_blank' href='mailto:jimmytidey@gmail.com'><img src='resources/gmail.png' alt='Gmail logo' /></a></p> 
+            <p><a target='_blank' href='http://twitter.com/#!/Jimmytidey'><img src='resources/twitter.png' alt ='Twitter logo'/></a></p>
+            <p><a target='_blank' href='https://github.com/jimmytidey'><img src='resources/github.png' alt='Github logo' id='github_logo' /></a></p>
+            <p><a target='_blank' href='/blog/'><img src='resources/blog_icon.png' alt='Blog logo' id='blog_logo' /></a></p>
         </div>
         
-        <div class='col-md-6 '>
-            
-            <p>I'm doing a <a href='http://thecreativeexchange.org/'>PhD at RCA</a>. I'm investigating new representations of online social interactions.</p>
-            
-            <h2 id='hypothesis'>Research Question</h2>
-            
-            <p>Most communities, for example a village or work place, produce a stream of 'digital exhaust' on services such as:</p>
-            
-            <ul>
-                <li>Facebook &amp; Twitter  &amp; Tumblr</li>
-                <li>Forums &amp; Blogs</li>
-                <li>Dropbox &amp; Google Docs</li>                
-            </ul>
-            
-            <p>My hypothesis is this data can be aggregated, processed and presented back to the community to assist in collaboration and decision making.</p>
-            
-            <p>There are three specific areas I will be looking at:</p>
+        <div id='arrow_left'>&laquo;</div>
+        
+        <div id='scroll_container'>
+            <div id='item_container'>
+                <div class='scroll_item' >
+                            
+                    <h2>I'm a freelance front-end developer and producer / product manager, my background spans both production and tech roles.</h2>        
+                            
+                    <p><strong>Tech skills</strong></p>
+                    <ul>    
+                        <li>Front-end development: Javascript/jQuery, HTML5, CSS, photoshop</li> 
+                        <li>PHP, WordPress, Git, Rails</li>
+                        <li>Semantic web technologies, such as <a target='_blank' href='http://jimmytidey.co.uk/blog/timeline-maker-com/'>SPARQL &amp; Freebase</a>. <a target='_blank' href='http://jimmytidey.co.uk/blog/lost-in-the-noise-what-we-really-think-about-musical-genres/'>Graph analysis</a>.</li>
+                    </ul>  
+                    <p><strong>Production skills</strong></p>
+                    <ul>
+                        <li>Defining roadmaps from conceptualisation to specification to delivery</li>
+                        <li>Writing specification documents, wireframing, making stimulus material for testing</li>
+                        <li>Running focus groups, surveys and user testing sessions</li>
+                        <li>Extensive experience of pitching</li> 
+                    </ul>                                   
+                    
+                    <img id='me' src='resources/me.png' alt='picture of me ' > 
+                    
+                </div>   
+                
+                <!-- New Scroll Item -->  
+                <div class='scroll_item' >
+                      
+                    <div class='freelance_nugget'>
+                        <img src='resources/bbc.png'  />
+                        <p><strong>BBC - Site for Orphan Black</strong></p>
+                        <p>jQuery / HTML5 site packaged so that it can be duplicated in every country that purchases the Orphan Black programme. </p>
+                    </div>
+                    
+                    <div class='freelance_nugget'>
+                        <img src='resources/lambeth.png'  />
+                        <p><strong>Lambeth Borough Council</strong><p> 
+                        <p><a href='http://feelgoodlambeth.org/'>Feelgood Lambeth</a> project. Using Ruby On Rails, Twitter Bootstrap, Heroku, Cloudinary image hosting, Google Maps API. <a href='https://github.com/jimmytidey/asset_mapping/'>Github</a>.</p>
+                    </div>
+                    
+                    
+                    <div class='freelance_nugget'>
+                        <img src='resources/rubric.png'  />
+                        <p><strong>Rubric Creative</strong><p> 
+                        <p>Working as a front-end web developer for Rubric Creative. Responsive Design and WordPress development. Example: <a href='http://trafalgarplace.com/'>Trafalgar Place</a>.</p>
+                    </div>
+                    
+                    <div class='freelance_nugget'>
+                        <img src='resources/n0tice.png'  />
+                        <p><strong>LBi - n0tice campaign for the Guardian</strong></p>
+                        <p>Front-end development using the N0tice API to produce <a href='http://about.n0tice.com/2012/10/30/lights-camera-action-its-n0tice-high-streets-the-movie/'>embeddable, dynamic infographics</a>.  </p>
+                    </div>
+                    
+                    <div class='freelance_nugget'>
+                        <img src='resources/nesta.png'  />
+                        <p><strong>Stian Westlake / NESTA</strong>   
+                        <p>Ongoing project to map the social graph of think tanks and index their research.  <a href='resources/thinktank_social_network.png' title='social networks' data-fancybox-group="gallery" class="fancybox">Little taster</a>. <a href='http://github.com/jimmytidey/think_tank_bank'>Github</a>.</p>
 
-            <ul>
-                <li><strong>Discovery:</strong> eg. printed highlights of hyperlocal blogs can reach people who do not read blogs online</li>
-                <li><strong>Motivation:</strong> eg. <a href='http://en.wikipedia.org/wiki/Calm_technology'>Sonification</a> of coworkers Tweets might nudge the team towards knowing more about what each other are thinking about</li>
-                <li><strong>Aggregation:</strong> A pattern might emerge from combining Twitter with blog posts that is otherwise invisible</li>                
-            </ul>
-            
-            <p>In my research I will be testing this hypothesis by building prototype systems.</p>    
-            
-            <h2 id='analogy'>An analogy</h2>
-            
-            <p>Architects plan offices to increase collaboration, or town planners seek to foster community by structuring public space - is there an equivalent intervention in digital space?</p>
-            
-            <p>My projects will explore building interfaces that cultivate cohesion in exactly the same way.</p>
-            
-            
-            <h2 id='prototyping'>Prototyping interfaces</h2>
-            
-            <p>My intention is to carry out rapid, iterative testing of different methods of aggregation, filtering and presentation. In order to achieve this I have built a flexible <a href='https://github.com/jimmytidey/obs'>software platform</a> for aggregation.</p>  
+                    </div>
 
-            <p>At the moment I'm developing a series of rapidly deployable ways to present the aggregated data. </p>
-
-            <p>The personal computer screen is the omni-present interface for all digital activities - from organising your calendar to writing to social activity. </p>  
+      
+                    <div class='freelance_nugget'>
+                         <img src='resources/se.png'  />
+                        <p ><strong>Somethin' Else</strong></p>
+                        <p>R&amp;D project producing maps of people's musical preferences using Facebook and YouTube.</p>
+                    </div>
+                    
+                </div>              
+                
+                
+                <!-- New Scroll Item -->  
+                <div class='scroll_item' >
+                    <img src='resources/evrythng_slider.png' />
+                    
+                    <div class='description'>
+                        <h1 class='tk-museo'>Evrythng</h1>
+                        <p class='question'>One day, your toaster and your guitar will be as connected as your phone. Evrythng want to provide a software platform to link together... well, everything.</p>
             
-            <img src='img/three_activities.png'  alt='Diagram of a single person using a single monitor to do three distinct digital activities' class='center' />
+                        <p>I worked as a Product Manager to investigate how the Evrythng platform fits into the nascent Internet of Things ecology.</p>
+                    
+                        <p>I produced prototypes using their API, ran user testing, surveys and participated in a their development sprints.</p>
                         
-            <p>I'm particularly interested in getting away from this paradigm, instead using a variety of intentionally public or shared interfaces, for example print, projection, shared screens. </p>
+                        <p>More <a target='_blank' href='http://evrythng.com'>here</a>.</p> 
+                    </div>
+                </div>      
+                
+                
+                <!-- New Scroll Item -->            
+                <div class='scroll_item' >
+                    <img src='resources/monterosa.png' />
+                    
+                    <div class='description'>
+                        <h1 class='tk-museo'>Monterosa</h1>
+                        <p class='question'>People increasingly have a computer to hand while they watch TV. Monterosa work to increase engagement by making laptops and tablets part of the TV experience.</p>
             
-            <img src='img/three_interface_three_person.png' alt='diagram of multiple people using multiple interfaces to see social digital activity' class='center' />
+                        <p>I worked as a Development Producer at Monterosa. My job was generating and refining ideas for websites that sync with TV programmes, then pitching them to TV Channels.</p>
+                    
+                        <p>Monterosa are leaders in '2-Screen' TV and worked with the BBC, ITV and Channel 4.</p>
                         
-            
-            <h2 id='work_so_far'>Work so far</h2>
-            
-            <h3>Kensington &amp; Chelsea Council</h3>
-            <img class='img_border' src="img/little_printer.png" alt="Little Printer printing out receipts">
-            <img class='img_border' src="img/receipt.png"  alt="Print out of social media activity">
-            <p>Receipt printer in the K&amp;C council staff canteen. We captured 8 stories about the borough from across Twitter, Facebook, Blogs and Forums every day and printed the headlines on the receipt, which short links to the full items</p>   
-            
-            <h3>FACT Gallery</h3>
-            <img class='img_border' src="img/fact.png" alt="Workshop at FACT gallery">    
-            <p>Asking cinema goers in the FACT gallery to respond to printed stories from Twitter &amp; Facebook. These were from the Bold Street area immediately surrounding the space. </p>
-            
-            <h3>Wonkbook</h3>
-            <img src="img/wonkbook.png" class='img_border'  alt="Wonkbook screen shot">
-            <p>Harvesting Twitter interactions between Think Tanks</p>
-            
-            <h3>CX Bucket</h3>
-            <img src="img/bucket.jpg" class='img_border' alt="Bucket">      
-            <p>Harvesting the digital exhaust of our office - sonification and screen</p>
-            
-            <h2 id='work_so_far'>Noun project credits</h2>
-            <ul>
-                <li><a href='http://thenounproject.com/matt.crum'>Matt Crum</a></li>
-                <li><a href='http://thenounproject.com/EricBirdDesign'>Eric Bird</a></li>
-                <li><a href='http://thenounproject.com/rudy321'>Rudy321</a></li>
-                <li><a href='http://thenounproject.com/Ashley5ash'>Ashley5ash</a></li>
-                <li><a href='http://thenounproject.com/jmkeuning'>Jm Keuning</a></li>
-                <li><a href='http://thenounproject.com/razerk'>Razerk</a></li>
-                <li><a href='http://thenounproject.com/jacopo.bonacci'>Jacopo Bonacci</a></li>
-                <li><a href='http://thenounproject.com/stefano.vetere'>Stefano Vetere</a></li>
-                <li><a href='http://thenounproject.com/bnvk'>Bnvk</a></li>  
-                <li><a href='http://thenounproject.com/Simon%20Child'>Simon Child</a></li>                                                                                                                                
-            </ul>
-            
+                        <p>You can see the Million Pound Drop app in action <a target='_blank' href='http://vimeo.com/25461004'>here</a>, and learn more about 2-Screen <a target='_blank' href='http://monterosa.co.uk/'>here</a>.</p>  
+                    </div>
+                </div>                      
+  
+                
+                <!-- New Scroll Item -->  
+                <div class='scroll_item' >
+                
+                    <div class='description'>
+                        <h1 class='tk-museo'>Personal Work</h1>
+                        <p>Here are three projects to demonstrate my tech skills.</p>
+                        
+                        <p><a href='http://heresay.org.uk'>Heresay</a> is a website that gathers forum posts from across London and makes them geographically searchable. It uses the responsive Twitter Bootstrap on the front end, and has a back end for scraping built in PHP. Code for the <a href='https://github.com/jimmytidey/heresay/'>Front end</a> and the <a href='https://github.com/jimmytidey/heresay_gather/'>Back end</a></p> 
+                        
+                        <p><a href='http://rifff.co.uk'>Rifff Audio Composer</a> is a rails projects, using Heroku hosting, Amazon S3, and HTML5 audio API. <a href='http://github.com/jimmytidey/rifff_rails'>Code on Github</a>.</p> 
+                
+                        <p><a href='http://bike-calculator.jimmytidey.co.uk/'>Bike Calculator</a> uses the HTML5 canvass tag via paper.js to provide an editable graph. <a href='https://github.com/jimmytidey/energy_caculator'>Code on Github</a>.</p> 
+                        
+                    </div>
+                </div>   
+                
+
+                
+                <!-- New Scroll Item -->  
+                <div class='scroll_item' >
+
+                    <div class='description'>
+                        <h1 class='tk-museo'>Get in contact</h1>
+                        
+                        <p>I'm available for hire.</p>
+                        
+                        <ul>
+                            <li>Twitter: <a target='_blank' href='https://twitter.com/Jimmytidey'>jimmytidey</a></li>
+                            <li><a target='_blank' href='mailto:jimmytidey@gmail.com'>jimmytidey@gmail.com</a></li>
+                        </ul>
+                    </div>    
+                </div>              
+                                                
+                    
+                
+            </div>    
         </div>
-    </div>
+        
+        <div id='arrow_right'>&raquo;</div>
+
+        <div id='guide'>
+            <p data-target='1'>About</p>
+            <p data-target='2'>Freelance work</p>
+            <p data-target='3'>Evrythng</p>
+            <p data-target='4'>Monterosa</p>
+            <p data-target='5'>Personal Work</p>                        
+            <p data-target='6'>Contact</p>
+        </div>
+        
+    </div>  
     
+    <script type="text/javascript">
 
-</div>
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-15474551-1']);
+      _gaq.push(['_trackPageview']);
 
-<?php  include 'footer.php' ?>
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
 
+    </script>   
     
-
-
+</body>
+</html>
