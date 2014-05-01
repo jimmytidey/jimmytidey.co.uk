5abd04d359ebb67edcbc69347c5c5c587be7567a
Jimmy <jimmy@Jimmy.(none)>
2014-05-01 13:23:05 +0100

    adding fb papers to methodology

--- html/phd/interim-exam/index.php
+++ html/phd/interim-exam/index.php
@@ -19,8 +19,9 @@ include('../../header.php');
                  <li><a href='../'>Overview for partners</a></li>
                  <li><a href="research.php">Research So Far</a></li>
                  <li><a href="https://docs.google.com/document/d/1RqOzBBMAVKS-AfpzA2LIzZiS3hPjh0kF9lnJ0nJ-RsE/edit#heading=h.sord9555il9y">RMC Essay</a></li>
-                 <li><a href="projects.php">Projects</a></li>
-                 <li><a href="conferences.php"></a></li>
+                 <li><a href="/phd/#work_so_far">Projects</a></li>
+                 <li><a href="conferences.php">Conferences attended</a></li>
+                 <li><a href="schedule.php">Schedule</a></li>
              </ul> 
        </div>
    </div>
--- html/phd/interim-exam/research.php
+++ html/phd/interim-exam/research.php
@@ -1,1 +1,106 @@
-research.php
+<?php 
+$title = 'Methodology';
+include('../../header.php');
+?>
+
+<div class='container'>
+	<div class='row'>
+		<div class='col-md-10 col-md-offset-1'>
+			<h1>Methodology</h1>
+		</div>
+
+	</div>
+
+	<div class='row'>
+		<div class='col-md-10 col-md-offset-1'>
+				
+			<p>My research question is whether visualising digital activity in shared spaces can enhance community cohesion.</p>
+
+			<p>In order to test this hypothesis I will use a number of projects to gather empirical data.</p>
+
+			<p>Making data visible in shared spaces evokes concepts of "calm technology" (Weiser &amp; Brown, 1997) or "ambient displays" (Wisneski et al, 1998). </p>
+
+			<h2>Using social capital as a foundation</h2>
+			
+			<p>Social capital has attractive properties as an evaluative framework for an investigation of the potential impact of ambient computing on communities.</p>  
+
+			<p>While there are many formulations of it, there is a consensus that all of them relate to "a resource to collective action" (Stone, 2001).</p>
+
+			<p>The ONS publishes data about social capital, and has a question bank for quantitative purposes. The OECD provides an official definition which is accepted by the world bank and the ONS: </p>
+
+			<p><blockquote>Networks together with shared norms, values and understandings that facilitate co-operation within or among groups.</blockquote></p>
+
+			<p>Measurement of social capital and its dimensions has been discussed at length in the literature. As examined later, there is a well established link between social capital, especially the 'bridging' dimension, and broader social effects, such as wellbeing and health. Additionally there is a nascent body of work connecting digital activity to social capital.</p> 
+
+			<img src="img/methodology_flow.png" alt="Methodology Flow" />
+
+
+
+			<h2>Step 1 - Quantitative data from projects.</h2>
+			
+			<h3>Experimental Design</h3>
+			<p>The effectiveness of experimental </p> 
+
+			<h3>Side Effects</h3>
+			<p></p>
+
+			<h3>Repeatability</h3> 
+
+
+
+			<h2>Step 2 - Qualitative evidence for enhanced social capital</h2>
+			<p></p>
+
+			
+
+			<h2>Step 2 - Correlation between social network activity and social capital in the literature</h2>
+			<p>Most research tends to agree that bridging social capital is more correlated with social media use than bonding social capital.</p>
+
+			<p>Perhaps the most prominent paper is "The benefits of Facebook “friends:” Social capital and college students' use of online social network sites" (Ellison et al, 2007), which has been cited nearly 3,500 times. In this paper they find that intensity of Facebook use is one of the strongest correlates of social capital, as determined by a standardised survey instrument.</p>
+			
+			<p>One of the most convincing papers on the topic (Burke, 2011) used Facebook adverts to gather ~1000 participants from across the English speaking world. Using privileged access to Facebook data (one of the authors is a Facebook employee), they were able to determine three modalities of Facebook use: direct messaging, passive consumption and broadcasting. Using two surveys of social capital taken one year apart they were then able to investigate correlations between changes in social capital and modalities of Facebook use, and found that all types of Facebook use, but especially direct messaging, correlate with bridging social capital (but no correlation with boding social capital).</p>
+
+			<p>An interesting aspect of this paper is that they also gathered data on major life events. This enabled them to qualify the scale of the effects, for example:</p>
+
+			<blockquote>“When compared to events, where we would expect a major disruption in social capital, the effect of Facebook is clear: every doubling of directed communication is associated with the same gain as moving to a new city, and about half the change incurred by job loss.</blockquote>
+
+			<p>In addition, they were able to determine the direction of causality by using life events as a control. Losing a job causes a decrease in social capital, but not a change in Facebook behavior. This suggests that Facebook behaviour causes changes in social capital, not the other way round.</p>
+
+			<h2>Step 3 - Correlation between social capital and community benefits</h2>
+
+			<p></p>
+			
+
+
+			
+			
+			<h2>Quantifying side effects</h2> 
+
+			
+			
+
+			<p><strong>Survey paper (2002)</strong></p>
+			<p>Stone, Wendy. Measuring social capital: Towards a theoretically informed measurement framework for researching social capital in family and community life. Vol. 24. Melbourne: Australian Institute of Family Studies, 2001.</p>
+			<p><em>Resource to collective action!</em></p>
+
+			
+
+
+			<h2>Similar work</h2>	
+
+			<h2>Communication Theory</h2>
+
+			<h2>Citations</h2>
+			<p>Weiser, Mark, and John Seely Brown. "The coming age of calm technology." Beyond calculation. Springer New York, 1997. 75-85.</p> 
+			<p>Wisneski, C., Ishii, H., Dahley, A., Gorbet, M., Brave, S., Ullmer, B., & Yarin, P. (n.d.). Ambient Displays : Turning Architectural Space into an Interface between People and Digital Information, 1–11.</p>
+			<p>Stone, W. (2001). Measuring social and community life, (24).</p>
+			<p>Ellison, Nicole B., Charles Steinfield, and Cliff Lampe. "The benefits of Facebook “friends:” Social capital and college students’ use of online social network sites." Journal of Computer‐Mediated Communication 12.4 (2007): 1143-1168.</p>
+			<p>Burke, Moira, Robert Kraut, and Cameron Marlow. "Social capital on Facebook: Differentiating uses and users." Proceedings of the SIGCHI Conference on Human Factors in Computing Systems. ACM, 2011.</p>
+		</div>
+</div>    
+
+<?php  include '../../footer.php' ?>
+
+
+
+
