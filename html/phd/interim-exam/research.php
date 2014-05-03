4995431c1c362e8a06ccd7e50a0ebf8d795b646e
Jimmy <jimmy@Jimmy.(none)>
2014-05-03 16:48:34 +0100

    framing causal chain as background for research

--- html/phd/interim-exam/research.php
+++ html/phd/interim-exam/research.php
@@ -1,3 +1,4 @@
+
 <?php 
 $title = 'Methodology';
 include('../../header.php');
@@ -5,55 +6,66 @@ include('../../header.php');
 
 <div class='container'>
 	<div class='row'>
-		<div class='col-md-10 col-md-offset-1'>
-			<h1>Methodology</h1>
+		<div class='col-md-9 col-md-offset-2'>
+			<h1>Research so far</h1>
 		</div>
 
 	</div>
 
+
+
 	<div class='row'>
-		<div class='col-md-10 col-md-offset-1'>
+
+        <div class='navbar col-md-2 '>
+            <ul class='nav'>
+            	<li><a href='#intro'>Intro</a></li>
+                <li><a href='#broad_context'>Broad Context</a></li>
+                <li><a href='#existing_methodologuies'>Existing Methodologies</a></li>
+                <li><a href='#analogy'>Experimental difficulties</a></li>
+                <li><a href='#prototyping'>Synthesised Methodology</a></li>          
+            </ul>
+        </div>
+
+		<div class='col-md-9 '>
+
+
+			<!-- INTRO --> 
 				
-			<p>My research question is whether visualising digital activity in shared spaces can enhance community cohesion.</p>
+			<p id='intro'>This project concerns ambient representation of digital activity associated with a physical space to enhance community cohesion.</p>
 
-			<p>In order to test this hypothesis I will use a number of projects to gather empirical data.</p>
+			<p>Here I will present the broader context of the research, which explains the motivation for the work. Assumptions about context will be supported through references to other papers</p>
 
-			<p>Making data visible in shared spaces evokes concepts of "calm technology" (Weiser &amp; Brown, 1997) or "ambient displays" (Wisneski et al, 1998). </p>
-
-			<h2>Using social capital as a foundation</h2>
+			<p>The wider background is also necessary to frame possible confounding factors and methodological difficulties.</p>
 			
-			<p>Social capital has attractive properties as an evaluative framework for an investigation of the potential impact of ambient computing on communities.</p>  
+			<p>I will then present a range existing methodologies as well as the problems I expect to encounter.</p>
 
-			<p>While there are many formulations of it, there is a consensus that all of them relate to "a resource to collective action" (Stone, 2001).</p>
-
-			<p>The ONS publishes data about social capital, and has a question bank for quantitative purposes. The OECD provides an official definition which is accepted by the world bank and the ONS: </p>
-
-			<p><blockquote>Networks together with shared norms, values and understandings that facilitate co-operation within or among groups.</blockquote></p>
-
-			<p>Measurement of social capital and its dimensions has been discussed at length in the literature. As examined later, there is a well established link between social capital, especially the 'bridging' dimension, and broader social effects, such as wellbeing and health. Additionally there is a nascent body of work connecting digital activity to social capital.</p> 
-
-			<img src="img/methodology_flow.png" alt="Methodology Flow" />
+			<p>Finally, I will synthesis these into a methodology that I can use for my own work.</p>
 
 
-
-			<h2>Step 1 - Quantitative data from projects.</h2>
+			<h2 id='broad_context'>Broad context</h2>
 			
-			<h3>Experimental Design</h3>
-			<p>The effectiveness of experimental </p> 
-
-			<h3>Side Effects</h3>
-			<p></p>
-
-			<h3>Repeatability</h3> 
-
-
-
-			<h2>Step 2 - Qualitative evidence for enhanced social capital</h2>
-			<p></p>
-
+			<img src="img/methodology_flow_2.png" alt="Methodology Flow" />
 			
+			<h3>Step 1 - Finding data for presentation.</h3>
 
-			<h2>Step 2 - Correlation between social network activity and social capital in the literature</h2>
+			<p>Two prototype versions of the system, demonstrated at FACT and Kensington & Chelsea Council Staff cafe were both able to find enough data to function.</p>
+
+			<p>Further, in the context of gathering 'hyperlocal' type data, a project with Hounslow and RSA has identified approximately 50 sources of useful online community data.</p>
+
+			<p>In the context of the office, the current CX 'bucket' project is finding at least enough data to power a working prototype.</p>
+
+
+			<h3>Step 2 - Presenting the data to drive users to interact.</h3>
+
+			<p>Dourish portholes project.</p>
+
+			<p>Calm technology</p>
+
+			<p>Social Mirror</p>
+
+
+			<h3>Step 3 - correlation between community-focused digital activity and social capital</h3>
+
 			<p>Most research tends to agree that bridging social capital is more correlated with social media use than bonding social capital.</p>
 
 			<p>Perhaps the most prominent paper is "The benefits of Facebook “friends:” Social capital and college students' use of online social network sites" (Ellison et al, 2007), which has been cited nearly 3,500 times. In this paper they find that intensity of Facebook use is one of the strongest correlates of social capital, as determined by a standardised survey instrument.</p>
@@ -66,29 +78,34 @@ include('../../header.php');
 
 			<p>In addition, they were able to determine the direction of causality by using life events as a control. Losing a job causes a decrease in social capital, but not a change in Facebook behavior. This suggests that Facebook behaviour causes changes in social capital, not the other way round.</p>
 
-			<h2>Step 3 - Correlation between social capital and community benefits</h2>
-
-			<p></p>
-			
+			<p>This effect has also been observed on Twitter (Hofer & Aubert, 2013) and on a hyperlocal community website (Littau, 2007).</p>
 
 
-			
-			
-			<h2>Quantifying side effects</h2> 
 
-			
-			
-
-			<p><strong>Survey paper (2002)</strong></p>
-			<p>Stone, Wendy. Measuring social capital: Towards a theoretically informed measurement framework for researching social capital in family and community life. Vol. 24. Melbourne: Australian Institute of Family Studies, 2001.</p>
-			<p><em>Resource to collective action!</em></p>
 
 			
 
+			<h3>Step 4 - Correlation between social capital and community benefits</h3>
+			<p>Social capital has attractive properties as an evaluative framework for an investigation of the potential impact of ambient computing on communities.</p>  
 
-			<h2>Similar work</h2>	
+			<p>While there are many formulations of it, there is a consensus that all of them relate to "a resource to collective action" (Stone, 2001).</p>
 
-			<h2>Communication Theory</h2>
+			<p>The ONS publishes data about social capital, and has a question bank for quantitative purposes. The OECD provides an official definition which is accepted by the world bank and the ONS: </p>
+
+			<p><blockquote>Networks together with shared norms, values and understandings that facilitate co-operation within or among groups.</blockquote></p>
+
+			<p>Measurement of social capital and its dimensions has been discussed at length in the literature. As examined later, there is a well established link between social capital, especially the 'bridging' dimension, and broader social effects, such as wellbeing and health. Additionally there is a nascent body of work connecting digital activity to social capital.</p> 
+
+			
+			<p>The Strength of Weak Ties (Granovetter, 1973) is referenced across widely in both ambient computing and the previously discussed analysis of social networks. </p>
+			
+			<p>His work suggests there are strong benefits to increases in social captial, and in particular, as the title suggests, in large collections of weak ties.</p>
+
+			<p>Stone gives a survey positive correlations with social capital that have been observed:</p>
+
+			<blockquote>life expectancy; health status; suicide rates; teenage pregnancy; crime rates; participation rates in tertiary education; employment and unemployment rates; family income; marital relationship formations and dissolutions; business confidence; job growth; growth in GDP; and balance of trade</blockquote>
+						
+
 
 			<h2>Citations</h2>
 			<p>Weiser, Mark, and John Seely Brown. "The coming age of calm technology." Beyond calculation. Springer New York, 1997. 75-85.</p> 
@@ -96,6 +113,8 @@ include('../../header.php');
 			<p>Stone, W. (2001). Measuring social and community life, (24).</p>
 			<p>Ellison, Nicole B., Charles Steinfield, and Cliff Lampe. "The benefits of Facebook “friends:” Social capital and college students’ use of online social network sites." Journal of Computer‐Mediated Communication 12.4 (2007): 1143-1168.</p>
 			<p>Burke, Moira, Robert Kraut, and Cameron Marlow. "Social capital on Facebook: Differentiating uses and users." Proceedings of the SIGCHI Conference on Human Factors in Computing Systems. ACM, 2011.</p>
+			<p>Hofer, M., & Aubert, V. (2013). Computers in Human Behavior Perceived bridging and bonding social capital on Twitter : Differentiating between followers and followees, 29, 2134–2142.</p>
+			<p>Littau, Jeremy. Citizen journalism and community building: predictive measures of social capital generation. Diss. University of Missouri--Columbia, 2007.</p>
 		</div>
 </div>    
 
