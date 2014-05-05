8effad0e66acaf16adb9a18b36bbd59eb1146a28
Jimmy <jimmy@Jimmy.(none)>
2014-05-05 20:47:49 +0100

    made affix work

--- html/phd/interim-exam/research.php
+++ html/phd/interim-exam/research.php
@@ -2,11 +2,11 @@
 <?php 
 $title = 'Methodology';
 include('../../header.php');
-?>
+?> 
 
 <div class='container'>
 	<div class='row'>
-		<div class='col-md-9 col-md-offset-2'>
+		<div class='col-md-9 col-md-offset-3'>
 			<h1>Research so far</h1>
 		</div>
 
@@ -16,28 +16,40 @@ include('../../header.php');
 
 	<div class='row'>
 
-        <div class='navbar col-md-2 '>
-            <ul class='nav'>
-            	<li><a href='#intro'>Intro</a></li>
-                <li><a href='#broad_context'>Broad Context</a></li>
-                <li><a href='#existing_methodologuies'>Existing Methodologies</a></li>
-                <li><a href='#analogy'>Experimental difficulties</a></li>
-                <li><a href='#prototyping'>Synthesised Methodology</a></li>
-                <li><a href='#citations'>Citations</a></li>          
-            </ul>
-        </div>
+		<div class='col-md-3'>
+			<nav id="affix-nav" >
+				<ul class="nav" data-spy="affix" data-offset-top="10">
 
-		<div class='col-md-9 '>
+					<li><a href='#intro'>Intro</a></li>
+					<li><a href='#structure'>Overall strucutre</a></li>
+					<li>
+						<ul>
+							<li><a href='#step_1'>Step 1: Gather the data</a></li>
+							<li><a href='#step_2'>Step 2: Represent data</a></li>
+							<li><a href='#step_3'>Step 3: Increase community participation</a></li>
+							<li><a href='#step_4'>Step 4: Generate Social Capital</a></li>
+						</ul>
+					</li>
+					<li><a href='#methodologies'>Methodologies</a></li>
+
+					<li><a href='#citations'>Citations</a></li>
+					<li><a href='#similar_work'>Appendix: Similar Work</a></li>	
+				</ul>
+
+			</nav>
+		</div>
+
+		<div class='col-md-8'>
 
 
 			<!-- INTRO --> 
-				
+
 			<p id='intro'>This project concerns ambient representation of digital activity associated with a physical space to enhance community cohesion.</p>
 
 			<p>Here I will present the broader context of the research, which explains the motivation for the work. Assumptions about context will be supported through references to other papers</p>
 
 			<p>The wider background is also necessary to frame possible confounding factors and methodological difficulties.</p>
-			
+
 			<p>I will then present a range existing methodologies as well as the problems I expect to encounter.</p>
 
 			<p>Finally, I will synthesis these into a methodology that I can use for my own work.</p>
@@ -45,13 +57,13 @@ include('../../header.php');
 
 			<!-- Broad Context --> 
 
-			<h2 id='broad_context'>Context and similar work in the literature</h2>
-			
+			<h2 id='structure'>Context and similar work in the literature</h2>
+
 			<img src="img/methodology_flow_2.png" alt="Methodology Flow" />
-			
+
 			<p><em>Steps 2 & 3 are the specific causal effects I want to investigate.</em></p> 
 
-			<h3>Step 1 - Finding data for presentation.</h3>
+			<h3 id='step_1'>Step 1 - Finding data for presentation.</h3>
 
 			<p>Two prototype versions of the system, demonstrated at FACT and Kensington & Chelsea Council Staff cafe were both able to find enough data to function.</p>
 
@@ -60,9 +72,58 @@ include('../../header.php');
 			<p>In the context of the office, the current CX 'bucket' project is finding at least enough data to power a working prototype.</p>
 
 
-			<h3>Step 2 - Ambient computing driving social awareness.</h3>
 
-			<p>Examples of projects using ambient <p>
+
+			<!-- step 1 --> 
+			<h3 id='step_1'>Step 2 - Ambient computing driving social awareness.</h3>
+
+			<p>Examples of projects using ambient </p>
+
+
+
+
+
+			<h3>Step 3 - correlation between community-focused digital activity and social capital</h3>
+
+			<p>Most research tends to agree that bridging social capital is more correlated with social media use than bonding social capital.</p>
+
+			<p>Perhaps the most prominent paper is "The benefits of Facebook “friends:” Social capital and college students' use of online social network sites" (Ellison et al, 2007), which has been cited nearly 3,500 times. In this paper they find that intensity of Facebook use is one of the strongest correlates of social capital, as determined by a standardised survey instrument.</p>
+
+			<p>One of the most convincing papers on the topic (Burke, 2011) used Facebook adverts to gather ~1000 participants from across the English speaking world. Using privileged access to Facebook data (one of the authors is a Facebook employee), they were able to determine three modalities of Facebook use: direct messaging, passive consumption and broadcasting. Using two surveys of social capital taken one year apart they were then able to investigate correlations between changes in social capital and modalities of Facebook use, and found that all types of Facebook use, but especially direct messaging, correlate with bridging social capital (but no correlation with boding social capital).</p>
+
+			<p>An interesting aspect of this paper is that they also gathered data on major life events. This enabled them to qualify the scale of the effects, for example:</p>
+
+			<blockquote>“When compared to events, where we would expect a major disruption in social capital, the effect of Facebook is clear: every doubling of directed communication is associated with the same gain as moving to a new city, and about half the change incurred by job loss.</blockquote>
+
+			<p>In addition, they were able to determine the direction of causality by using life events as a control. Losing a job causes a decrease in social capital, but not a change in Facebook behavior. This suggests that Facebook behaviour causes changes in social capital, not the other way round.</p>
+
+			<p>This effect has also been observed on Twitter (Hofer & Aubert, 2013) and on a hyperlocal community website (Littau, 2007).</p>
+
+
+
+			<h3>Step 4 - Correlation between social capital and community benefits</h3>
+
+			<p>Social capital has attractive properties as an evaluative framework for an investigation of the potential impact of ambient computing on communities.</p>  
+
+			<p>While there are many formulations of it, there is a consensus that all of them relate to "a resource to collective action" (Stone, 2001).</p>
+
+			<p>The ONS publishes data about social capital, and has a question bank for quantitative purposes. The OECD provides an official definition which is accepted by the world bank and the ONS: </p>
+
+			<blockquote>Networks together with shared norms, values and understandings that facilitate co-operation within or among groups.</blockquote>
+
+			<p>Measurement of social capital and its dimensions has been discussed at length in the literature. As examined later, there is a well established link between social capital, especially the 'bridging' dimension, and broader social effects, such as wellbeing and health. Additionally there is a nascent body of work connecting digital activity to social capital.</p> 
+
+
+			<p>The Strength of Weak Ties (Granovetter, 1973) is referenced across widely in both ambient computing and the previously discussed analysis of social networks. </p>
+
+			<p>His work suggests there are strong benefits to increases in social captial, and in particular, as the title suggests, in large collections of weak ties.</p>
+
+			<p>Stone gives a survey positive correlations with social capital that have been observed:</p>
+
+			<blockquote>life expectancy; health status; suicide rates; teenage pregnancy; crime rates; participation rates in tertiary education; employment and unemployment rates; family income; marital relationship formations and dissolutions; business confidence; job growth; growth in GDP; and balance of trade</blockquote>
+
+
+			<h2 id='similar_work'>Appendix: Similar Work</h2>
 
 			<table class="table table-striped">
 
@@ -73,7 +134,7 @@ include('../../header.php');
 						<th>Other intervention</th>
 					</tr>
 				</thead>
-				
+
 
 				<tr>
 					<td>Private Screen<br/>(Laptop or Desktop)</td>
@@ -105,12 +166,17 @@ include('../../header.php');
 						<p>Enticing People to Interact with Large Public Displays in Public Spaces (2002)</p>
 						<p>Interactive Community Bulletin Boards as Conversational Hubs and Sites for Playful Visual Repartee (2007)</p>
 						<p>Agora2 . 0 : Enhancing Civic Participation through a Public Display(2013)</p>
-						<p>Enticing People to Interact with Large Public Displays in Public Spaces (2002)</p>
-						<p>Enticing People to Interact with Large Public Displays in Public Spaces (2002)</p>
+						<p>Social Networked Displays : Integrating Networked Public Displays with Social Media (2013)</p>
+						<p>Moment Machine : Opportunities and Challenges of Posting Situated Snapshots onto Networked Public Displays (2013)</p>
+						<p>Designing Interaction for Local Communications: An Urban Screen Study (2013)</p>
+						<p>Supporting Community Awareness with Interactive Displays (2012)</p>
+						<p>Probing Communities : Study of a Village Photo Display (2007)</p>
+						<p>MyPosition : Sparking Civic Discourse by a Public Interactive Poll Visualization (2014)</p>
+
 					</td>
 					<td></td>
 				</tr>
-				
+
 				<tr>
 					<td>Public Screen (Broadcast)</td>
 					<td><p>Computation for the Corridor : Experiments in Augmenting Public Spaces (2014)</p></td>
@@ -120,54 +186,20 @@ include('../../header.php');
 
 				<tr>
 					<td>Ambient interface</td>
-					<td></td>
-					<td></td>
+					<td>
+						<p>http://berglondon.com/projects/availabot/</p>
+
+					</td>
+					<td>
+						<p>ambientROOM : Integrating Ambient Media with Architectural Space (1998)</p>
+						<p>Breakaway : An Ambient Display Designed to Change Human Behavior (2005)</p>
+						<p>Ambient Influence : Can Twinkly Lights Lure and Abstract Representations Trigger Behavioral Change ? (2010)</p>
+					</td>
 				</tr>
 
 			</table>
 
 
-
-
-			<h3>Step 3 - correlation between community-focused digital activity and social capital</h3>
-
-			<p>Most research tends to agree that bridging social capital is more correlated with social media use than bonding social capital.</p>
-
-			<p>Perhaps the most prominent paper is "The benefits of Facebook “friends:” Social capital and college students' use of online social network sites" (Ellison et al, 2007), which has been cited nearly 3,500 times. In this paper they find that intensity of Facebook use is one of the strongest correlates of social capital, as determined by a standardised survey instrument.</p>
-			
-			<p>One of the most convincing papers on the topic (Burke, 2011) used Facebook adverts to gather ~1000 participants from across the English speaking world. Using privileged access to Facebook data (one of the authors is a Facebook employee), they were able to determine three modalities of Facebook use: direct messaging, passive consumption and broadcasting. Using two surveys of social capital taken one year apart they were then able to investigate correlations between changes in social capital and modalities of Facebook use, and found that all types of Facebook use, but especially direct messaging, correlate with bridging social capital (but no correlation with boding social capital).</p>
-
-			<p>An interesting aspect of this paper is that they also gathered data on major life events. This enabled them to qualify the scale of the effects, for example:</p>
-
-			<blockquote>“When compared to events, where we would expect a major disruption in social capital, the effect of Facebook is clear: every doubling of directed communication is associated with the same gain as moving to a new city, and about half the change incurred by job loss.</blockquote>
-
-			<p>In addition, they were able to determine the direction of causality by using life events as a control. Losing a job causes a decrease in social capital, but not a change in Facebook behavior. This suggests that Facebook behaviour causes changes in social capital, not the other way round.</p>
-
-			<p>This effect has also been observed on Twitter (Hofer & Aubert, 2013) and on a hyperlocal community website (Littau, 2007).</p>
-
-			
-
-			<h3>Step 4 - Correlation between social capital and community benefits</h3>
-
-			<p>Social capital has attractive properties as an evaluative framework for an investigation of the potential impact of ambient computing on communities.</p>  
-
-			<p>While there are many formulations of it, there is a consensus that all of them relate to "a resource to collective action" (Stone, 2001).</p>
-
-			<p>The ONS publishes data about social capital, and has a question bank for quantitative purposes. The OECD provides an official definition which is accepted by the world bank and the ONS: </p>
-
-			<p><blockquote>Networks together with shared norms, values and understandings that facilitate co-operation within or among groups.</blockquote></p>
-
-			<p>Measurement of social capital and its dimensions has been discussed at length in the literature. As examined later, there is a well established link between social capital, especially the 'bridging' dimension, and broader social effects, such as wellbeing and health. Additionally there is a nascent body of work connecting digital activity to social capital.</p> 
-
-			
-			<p>The Strength of Weak Ties (Granovetter, 1973) is referenced across widely in both ambient computing and the previously discussed analysis of social networks. </p>
-			
-			<p>His work suggests there are strong benefits to increases in social captial, and in particular, as the title suggests, in large collections of weak ties.</p>
-
-			<p>Stone gives a survey positive correlations with social capital that have been observed:</p>
-
-			<blockquote>life expectancy; health status; suicide rates; teenage pregnancy; crime rates; participation rates in tertiary education; employment and unemployment rates; family income; marital relationship formations and dissolutions; business confidence; job growth; growth in GDP; and balance of trade</blockquote>
-						
 
 
 			<h2 id='citations'>Citations</h2>
@@ -179,10 +211,8 @@ include('../../header.php');
 			<p>Hofer, M., & Aubert, V. (2013). Computers in Human Behavior Perceived bridging and bonding social capital on Twitter : Differentiating between followers and followees, 29, 2134–2142.</p>
 			<p>Littau, Jeremy. Citizen journalism and community building: predictive measures of social capital generation. Diss. University of Missouri--Columbia, 2007.</p>
 		</div>
-</div>    
+	</div>    
+</div>
+
 
 <?php  include '../../footer.php' ?>
-
-
-
-
