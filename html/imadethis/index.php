5089006fe913c135dbadf70908b1c8ae1b32752c
Jimmy Tide <jimmytidey@gmail.com>
2014-12-09 10:44:27 +0000

    I made thiz

---
+++ html/imadethis/index.php
@@ -0,0 +1,26 @@
+
+<?php 
+$title = 'Jimmy Tidey';
+include('../header.php');
+?>
+
+<div class='container'>
+	<div class='row'>
+		
+		<h1>Hounslow Complete</h1>
+		<img src='big.png'>
+
+		<h1>Hounslow - most linked</h1>
+		<img src='small.png'>
+		
+		<h1>Peterborough Heatmap</h1>
+		<img src='heat.png'> 
+	</div>
+</div>
+
+<?php  include '../footer.php' ?>
+
+
+
+
+w
