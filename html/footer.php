4eaf05536d78d166c6ae4f3e8087a02a6e8a4494
Jimmy Tidey <jimmytidey@gmail.com>
2016-02-15 19:11:00 +0000

    front page copy (again)

--- html/footer.php
+++ html/footer.php
@@ -1,8 +1,8 @@
         <div class="container"> 
             <footer class='row'>
-                <div class='col-md-4 col-md-offset-4 '>
+                <div class='col-md-8 col-md-offset-2 footer '>
                     <p id='contact'>jimmy.tidey@network.rca.ac.uk</p>
-                    <p><a href='/'>home</a></p>
+                   
                 </div>
             </footer>
         </div> 
--- html/index.php
+++ html/index.php
@@ -6,52 +6,45 @@ include('header.php');
 
 <div class='container'>
 	<div class='row'>
-		<div id='background' class='col-md-7 col-md-offset-3'>
+		<div id='background' class='col-md-8 col-md-offset-2'>
 			<h1>Jimmy Tidey</h1>
 		</div>
 	</div>
 
 	<div class='row'>
-		<div class=' col-md-2 col-md-offset-1'>
+
+
+		<div class='col-md-6 col-md-offset-2 '>
+		  
+      
+			<p>I'm Jimmy, I'm in my final year of a PhD at The Royal College of Art.</p> 
+
+      <p>Before RCA I worked in tech. Favourite gigs include: <a href='http://www.bbc.co.uk/labuk/'>citizen science at the BBC</a>, integrating <a href='http://www.monterosa.co.uk/'>online games into live TV</a>, creative developer at an <a href='https://evrythng.com/'>IoT startup</a>.</p>
+      
+      <p>My PhD is about using social media to make pubic policy more responsive and inclusive. 
+        
+      <p>Companies like AirBnB &amp; Zipcar are incredibly successful at matching fragmentary capacity &amp; needs. Could we design something similar to help the public sector be as nimble?</p>
+
+      <p>I've developed <a href='https://localnets.org'>Localnets.org</a> - a tool which helps find the capacities and needs citizens express on Twitter. It's been used in research with NESTA and The RSA, and is being piloted at NHS Birmingham.</p> 
+
+      <p>From a theoretical perspective, I'm drawing on economics, network science &amp; sociology to think about designing new ways of collaborating, sharing &amp; coordinating.</p> 
+      
+      <p>My blog records the various <a href=''>hackdays</a>, <a href=''>conferences</a>, &amp; <a href=''>uncategorisable happenings</a> that I've organised or participated in. I'm especially interested in collaborations that cross boundaries between the sciences &amp; humanities (or however you want to construe that divide).</p>  
+      
+     
+
+      
+
+		</div>
+		
+		<div class=' col-md-2 '>
 			<ul class='nav'>
 				<li><a href='http://twitter.com/jimmytidey'>Twitter</a></li>
 				<li><a href='/blog'>Blog</a></li>
 				<li><a href='http://github.com/jimmytidey'>GitHub</a></li>
-				<li><a href='/phd/'>Employment</a></li>    
+				<li><a href='https://www.linkedin.com/in/jimmytidey'>LinkedIn</a></li>    
 			</ul>
-		</div>
-
-		<div class='col-md-7 '>
-      
-			<p>I'm Jimmy. I'm doing a PhD at The Royal College of Art. My research question is: How can tech can help us cooperate, share and collaborate? In other words, what new kinds of '<a href='https://en.wikipedia.org/wiki/Collective_action'>social coordination</a>' are emerging?</p> 
-      
-      <p>Are 'sharing economy' platforms or civic innovations like Fix My Street ethical? Could we design them to be more effective? Using ideas from economics, network science and design I'm researching digital platforms for social coordination.</p>   
-        
-      <p>It's not all ivory tower, I've developed <a href='https://localnets.org'>Localnets.org</a> - which helps local government understand what citizens are saying on Twitter, currently in use with NHS Birmingham.</p> 
-      
-      <p>My background is project management and programming. I've worked on citizen science at the BBC, integrating online games with Live TV, and at an IoT startup.</p>
-      
-      <p>My blog records the various hackdays, conferences, and uncategorisable happenings that I've organised or participated in. I'm especially interested in collaborations that cross boundaries between sciences and humanities (or however you want to construe that divide).</p>  
-      
-     
-        
-      <hr />
-      
-      <p>Localnets</p>
-      
-      <hr />
-      
-      <p>Art Hackathon</p>
-      
-      <hr />
-      
-      <p>Meta Londoners</p>
-      
-      <hr/>
-      
-      
-
-		</div>
+		</div>		
 	</div>
 </div>
 
@@ -62,7 +55,3 @@ include('header.php');
 
 <?php  include 'footer.php' ?>
 
-
-
-
-w
