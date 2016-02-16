36cf8911a3614bb9af4ac95c44dd80994baecf83
Jimmy Tidey <jimmytidey@gmail.com>
2016-02-16 08:32:22 +0000

    copy for front page

--- html/index.php
+++ html/index.php
@@ -19,15 +19,13 @@ include('header.php');
       
 			<p>I'm Jimmy, I'm in my final year of a PhD at The Royal College of Art.</p> 
 
-      <p>Before RCA I worked in tech. Favourite gigs include: <a href='http://www.bbc.co.uk/labuk/'>citizen science at the BBC</a>, integrating <a href='http://www.monterosa.co.uk/'>online games into live TV</a>, creative developer at an <a href='https://evrythng.com/'>IoT startup</a>.</p>
+      <p>Before RCA I worked in tech. Before that I did a degree in physics &amp; philosophy. Favourite gigs include: <a href='http://www.bbc.co.uk/labuk/'>citizen science at the BBC</a>, integrating <a href='http://www.monterosa.co.uk/'>online games into live TV</a>, creative developer at an <a href='https://evrythng.com/'>IoT startup</a>.</p>
       
-      <p>My PhD is about using social media to make pubic policy more responsive and inclusive. 
-        
-      <p>Companies like AirBnB &amp; Zipcar are incredibly successful at matching fragmentary capacity &amp; needs. Could we design something similar to help the public sector be as nimble?</p>
+      <p>My PhD is about using social media to make public policy more responsive and inclusive.  Brands monitor social media to understand what customers say about their products, could the public sector be as nimble?</p>
 
-      <p>I've developed <a href='https://localnets.org'>Localnets.org</a> - a tool which helps find the capacities and needs citizens express on Twitter. It's been used in research with NESTA and The RSA, and is being piloted at NHS Birmingham.</p> 
+      <p>This thought led to <a href='https://localnets.org'>Localnets.org</a> - a tool which helps local government find out what a community is saying on Twitter. It's been used in research with <a href='http://www.nesta.org.uk/publications/community-mirror-data-driven-method-below-radar-research'>NESTA</a> and <a href='https://www.thersa.org/discover/publications-and-articles/reports/community-capital-the-value-of-connected-communities/'>The RSA</a>, and is being piloted at NHS Birmingham.</p> 
 
-      <p>From a theoretical perspective, I'm drawing on economics, network science &amp; sociology to think about designing new ways of collaborating, sharing &amp; coordinating.</p> 
+      <p>From a theoretical perspective, I'm drawing on economics, network science &amp; design to think about new ways of collaborating, sharing &amp; coordinating.</p> 
       
       <p>My blog records the various <a href=''>hackdays</a>, <a href=''>conferences</a>, &amp; <a href=''>uncategorisable happenings</a> that I've organised or participated in. I'm especially interested in collaborations that cross boundaries between the sciences &amp; humanities (or however you want to construe that divide).</p>  
       
@@ -42,7 +40,9 @@ include('header.php');
 				<li><a href='http://twitter.com/jimmytidey'>Twitter</a></li>
 				<li><a href='/blog'>Blog</a></li>
 				<li><a href='http://github.com/jimmytidey'>GitHub</a></li>
-				<li><a href='https://www.linkedin.com/in/jimmytidey'>LinkedIn</a></li>    
+				<li><a href='https://www.linkedin.com/in/jimmytidey'>LinkedIn</a></li>
+				<li><a href='https://pinboard.in/u:jimmytidey?mode=list&floor=2 '>Bookmarks</a></li>
+				
 			</ul>
 		</div>		
 	</div>
