01e6c32e395216ff42e6d4218c7d374c97375806
Jimmy Tidey <jimmytidey@gmail.com>
2016-02-24 13:54:42 +0000

    adding projects to front page

--- html/index.php
+++ html/index.php
@@ -21,14 +21,55 @@ include('header.php');
 
       <p>Before RCA I worked in tech, and before that I did a degree in physics &amp; philosophy. Favourite gigs include: <a href='http://www.bbc.co.uk/labuk/'>citizen science at the BBC</a>, integrating <a href='http://www.monterosa.co.uk/'>online games into live TV</a>, creative developer at an <a href='https://evrythng.com/'>IoT startup</a>.</p>
       
-      <p>In my research I'm using the design context to bring my experience of delivering digital products together with theoretical approaches from network science, economics and policy.</p>
+      <p>In my research I'm bringing my experience of delivering digital products together with theoretical approaches from network science, economics and policy. I'm building digital tools for new kinds of collaboration and coordination.</p>
       
       <p>My focus is using social media to make public policy more responsive and inclusive.  Brands monitor social media to understand what customers say about their products, could the public sector be as nimble?</p>
 
-      <p>I have developed <a href='https://localnets.org'>Localnets.org</a> - a tool which helps local government find out what a community is saying on Twitter. It's been used in research with <a href='http://www.nesta.org.uk/publications/community-mirror-data-driven-method-below-radar-research'>NESTA</a> and <a href='https://www.thersa.org/discover/publications-and-articles/reports/community-capital-the-value-of-connected-communities/'>The RSA</a>, and is being piloted at NHS Birmingham.</p> 
+      <p>I have developed <a href='http://localnets.org/'>Localnets.org</a> - a tool which helps local government find out what a community is saying on Twitter. It's been used in research with <a href='http://www.nesta.org.uk/publications/community-mirror-data-driven-method-below-radar-research'>NESTA</a> and <a href='https://www.thersa.org/discover/publications-and-articles/reports/community-capital-the-value-of-connected-communities/'>The RSA</a>, and is being piloted at NHS Birmingham.</p> 
 
       <p>My blog records the various <a href='http://jimmytidey.co.uk/blog/category/hack-days/'>hackdays</a>, <a href='http://jimmytidey.co.uk/blog/category/conferences/'>conferences</a>, &amp; <a href='http://jimmytidey.co.uk/blog/lote4-the-stewardship-matera/'>uncategorisable happenings</a> that I've organised or participated in.</p>  
       
+      <h3>Selected projects</h3>
+      
+      <hr/>
+      
+      <a href='http://localnets.org'><img src='img/localnets.png' class='portfolio_image' ></a>
+      <p><a href='http://localnets.org'>Localnets.org</a> is an online tool to find active local citizens &amp; build democratic engagement. Click through to the website to find detailed descriptions and case studies.</p>
+      <p><a href='http://www.nesta.org.uk/publications/community-mirror-data-driven-method-below-radar-research'>It is the subject of this Nesta working paper</a>.</p>
+      <br style='clear:both' />
+      <hr />
+      
+      <a href='http://wonkbook.io'><img src='img/wonkbook.png' class='portfolio_image' ></a>
+      <p><a href='http://wonkbook.io'>Wookbook</a> uses Twitter to evaluate the networks that form between think tanks, politicians and journalists. Every week it creates a league table of the most impactful tweeters, and shows how ideas are spreading across the network.</p>
+      
+      <br style='clear:both' />
+      <hr/>      
+      
+      
+      <a href='http://arthackathon.co.uk'><img src='img/arthack.png' class='portfolio_image' ></a>
+      <p><a href='http://arthackathon.co.uk'>Arthackathon</a> was an event sponsored by Ravensbourne University. It brought over 50 artists and coders together to create exhibitable artworks over a weekend. The Localnets tool was used to evaluate changes in the participants social networks.</p>
+      
+      <br style='clear:both' />
+      <hr/>
+      
+      <img src='img/acrossrca.png' class='portfolio_image' >
+      <p>I helped run Metalonders, a week long, cross disciplinary workshop for students joining the RCA. We investigated narratives and social media in four sites across London, producing alternative maps of each location.</p>
+      
+      <br style='clear:both' />
+      <hr/>
+      
+      <a href='http://www.bbc.co.uk/labuk/results/braintestbritain/'><img src='img/braintrain.png' class='portfolio_image' style='margin-bottom:50px' ></a>
+      <p>While at the BBC I worked on <a href='http://www.bbc.co.uk/labuk/results/braintestbritain/'>Brain Test Britain</a>, an online experiment to see if brain training works (it doesn't!) with over 100,000 participants. The work resulted in a <a href='http://www.nature.com/news/2010/100420/full/4641111a.html
+      '>paper in the journal Nature</a>, and was one of the first psychology experiments carried out on the web.</p>
+      
+      <br style='clear:both' />
+      <hr/>
+      
+      <a href='http://www.monterosa.co.uk/cases/mpd'><img src='img/mpd.png' class='portfolio_image' ></a>
+      <p>I worked on integrating an online game with Channel 4's <a href='http://www.monterosa.co.uk/cases/mpd'>Million Pound Drop TV show</a>. It was one of the most successful integrations of its type, on some occasions over 10% of TV viewers were also playing along online.</p>
+      
+      <br style='clear:both' />
+      <hr/>            
       
 
 		</div>
