9506b79ad2e89c7ed1ade4e6e7deb9e86bcb5900
Jimmy Tidey <jimmytidey@gmail.com>
2017-10-18 17:39:03 +0100

    ammend

--- html/index.php
+++ html/index.php
@@ -17,23 +17,20 @@ include('header.php');
 		<div class='col-md-6 col-md-offset-2 '>
 		  
       
-	<p>I'm Jimmy, I'm in my final year of a PhD at The Royal College of Art.</p> 
+	<p>I'm Jimmy, I'm awaiting my viva for my PhD at The Royal College of Art.</p> 
 
-      <p>Before RCA I worked in tech, and before that I did a degree in physics &amp; philosophy. Favourite gigs include: <a href='http://www.bbc.co.uk/labuk/'>citizen science at the BBC</a>, integrating <a href='http://www.monterosa.co.uk/'>online games into live TV</a>, creative developer at an <a href='https://evrythng.com/'>IoT startup</a>.</p>
+      <p>Before starting my PhD I worked in tech, and before that I did a degree in physics &amp; philosophy. Favourite gigs include: <a href='http://www.bbc.co.uk/labuk/'>citizen science at the BBC</a>, integrating <a href='http://www.monterosa.co.uk/'>online games into live TV</a>, creative developer at an <a href='https://evrythng.com/'>IoT startup</a>.</p>
       
-      <p>In my research I'm bringing my experience of delivering digital products together with theoretical approaches from network science, economics and policy - building digital tools for new kinds of collaboration and coordination.</p>
-      
-      <p>My focus is using social media to make public policy more responsive and inclusive.  Brands monitor social media to understand what customers say about their products, could the public sector be as nimble?</p>
-
-      <p>I have developed <a href='http://localnets.org/'>Localnets.org</a> - a tool which helps local government find out what a community is saying on Twitter. It's been used in research with <a href='http://www.nesta.org.uk/publications/community-mirror-data-driven-method-below-radar-research'>NESTA</a> and <a href='https://www.thersa.org/discover/publications-and-articles/reports/community-capital-the-value-of-connected-communities/'>The RSA</a>, and is being piloted at NHS Birmingham.</p> 
+      <p>My research bringings together my experience of developing digital products and theoretical approaches from network science, economics and public policy.</p>
 
       <p>My blog records the various <a href='http://jimmytidey.co.uk/blog/category/hack-days/'>hackdays</a>, <a href='http://jimmytidey.co.uk/blog/category/conferences/'>conferences</a>, &amp; <a href='http://jimmytidey.co.uk/blog/lote4-the-stewardship-matera/'>uncategorisable happenings</a> that I've organised or participated in.</p>  
+
       
       <h3>Selected projects</h3>
 
 
       <a href='http://localnets.org'><img src='img/localnets.png' class='portfolio_image' ></a>
-      <p>LocalNets is a Twitter Analytics tool aimed at Local Government and other non-market institutions. I developed LocalNets in the course of my PhD, and it supported all of my field work.</p>
+      <p>LocalNets is a Twitter Analytics tool aimed at Local Government and other non-market institutions that support communities. I developed LocalNets in the course of my PhD, and it supported all of my field work.</p>
 
       <p>It is based on Mongo, Node &amp; Neo4J, and has been used with NHS Birmingham, the RSA, DemSoc, NESTA, DCLG and in numerous other projects.</p> 
 
@@ -57,7 +54,7 @@ include('header.php');
       <!-- *************************** -->
 
       <a href='https://xkcd.com/770/'><img src='img/xkcd.png' class='portfolio_image' ></a>
-      <p>The RCA architecture department wanted a method to match students and tutors based on polls of both groups’ preferences. I implemented the Gale-Shapley algorithm, which achieves an optimal matching. Image from <a href='https://xkcd.com/770/'>XKCD</a>. 
+      <p>The RCA architecture department wanted a method to match students with tutors based on polls of both groups’ preferences. I implemented the Gale-Shapley algorithm, which achieves an optimal matching. Image from <a href='https://xkcd.com/770/'>XKCD</a>. 
       </p>
       <br style='clear:both' />
       <hr />
@@ -65,7 +62,7 @@ include('header.php');
       <!-- *************************** -->
       
       <a href='https://edgeryders.eu/t/mapping-the-opencare-ecosystem-using-twitter/5604/6'><img src='img/opencare.png' class='portfolio_image' ></a>
-      <p><a href='https://edgeryders.eu/t/mapping-the-opencare-ecosystem-using-twitter/5604/6'>OpenCare</a> investigated institutions that provide healthcare outside of the public sector. LocalNets mapped Twitter activity around projects developing open source insulin pumps, 3D printed prosthetics &amp; smartphone based healthcare in developing countries. </p><p> It contributed to Edgeryders report on open healthcare innovation, <a href='https://edgeryders.eu/c/opencare'>commissioned by the EU</a>. </p>
+      <p><a href='https://edgeryders.eu/t/mapping-the-opencare-ecosystem-using-twitter/5604/6'>OpenCare</a> investigated institutions that provide healthcare outside of the public sector. LocalNets mapped Twitter activity around projects developing open source insulin pumps, 3D printed prosthetics &amp; smartphone based healthcare in developing countries. </p><p> It contributed to Edgeryders' report on open healthcare innovation, <a href='https://edgeryders.eu/c/opencare'>commissioned by the EU</a>. </p>
      
       <br style='clear:both' />
       <hr />
@@ -82,7 +79,7 @@ include('header.php');
       <!-- *************************** -->
       
       <a href='https://www.thersa.org/discover/publications-and-articles/reports/community-capital-the-value-of-connected-communities'><img src='img/rsa.jpg' class='portfolio_image' ></a>
-      <p><a href='https://www.thersa.org/discover/publications-and-articles/reports/community-capital-the-value-of-connected-communities'>Connected Communities report with the RSA</a>. The LocalNets Twitter analytics tool was used to map communities in Peterborough and Hounslow. The data was combined with detailed ethnographic surveys carried out the RSA to contribute to their Connected Communities report.  
+      <p><a href='https://www.thersa.org/discover/publications-and-articles/reports/community-capital-the-value-of-connected-communities'>Connected Communities report with the RSA</a>.  LocalNets was used to map communities in Peterborough and Hounslow. The data was combined with detailed ethnographic surveys carried out the RSA to contribute to their Connected Communities report.  
       </p>
      
       <br style='clear:both' />
