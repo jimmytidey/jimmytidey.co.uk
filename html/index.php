f4ce902f5a80cb1802fae989b0031978a1c6873f
Jimmy Tidey <jimmytidey@gmail.com>
2017-10-18 15:58:16 +0100

    end of phd update

--- html/index.php
+++ html/index.php
@@ -17,11 +17,11 @@ include('header.php');
 		<div class='col-md-6 col-md-offset-2 '>
 		  
       
-			<p>I'm Jimmy, I'm in my final year of a PhD at The Royal College of Art.</p> 
+	<p>I'm Jimmy, I'm in my final year of a PhD at The Royal College of Art.</p> 
 
       <p>Before RCA I worked in tech, and before that I did a degree in physics &amp; philosophy. Favourite gigs include: <a href='http://www.bbc.co.uk/labuk/'>citizen science at the BBC</a>, integrating <a href='http://www.monterosa.co.uk/'>online games into live TV</a>, creative developer at an <a href='https://evrythng.com/'>IoT startup</a>.</p>
       
-      <p>In my research I'm bringing my experience of delivering digital products together with theoretical approaches from network science, economics and policy. I'm building digital tools for new kinds of collaboration and coordination.</p>
+      <p>In my research I'm bringing my experience of delivering digital products together with theoretical approaches from network science, economics and policy - building digital tools for new kinds of collaboration and coordination.</p>
       
       <p>My focus is using social media to make public policy more responsive and inclusive.  Brands monitor social media to understand what customers say about their products, could the public sector be as nimble?</p>
 
@@ -30,14 +30,67 @@ include('header.php');
       <p>My blog records the various <a href='http://jimmytidey.co.uk/blog/category/hack-days/'>hackdays</a>, <a href='http://jimmytidey.co.uk/blog/category/conferences/'>conferences</a>, &amp; <a href='http://jimmytidey.co.uk/blog/lote4-the-stewardship-matera/'>uncategorisable happenings</a> that I've organised or participated in.</p>  
       
       <h3>Selected projects</h3>
-      
-      <hr/>
-      
+
+
       <a href='http://localnets.org'><img src='img/localnets.png' class='portfolio_image' ></a>
-      <p><a href='http://localnets.org'>Localnets.org</a> is an online tool to find active local citizens &amp; build democratic engagement. Click through to the website to find detailed descriptions and case studies.</p>
-      <p><a href='http://www.nesta.org.uk/publications/community-mirror-data-driven-method-below-radar-research'>It is the subject of this Nesta working paper</a>.</p>
+      <p>LocalNets is a Twitter Analytics tool aimed at Local Government and other non-market institutions. I developed LocalNets in the course of my PhD, and it supported all of my field work.</p>
+
+      <p>It is based on Mongo, Node &amp; Neo4J, and has been used with NHS Birmingham, the RSA, DemSoc, NESTA, DCLG and in numerous other projects.</p> 
+
       <br style='clear:both' />
       <hr />
+
+      <!-- *************************** -->     
+
+
+
+      <a href='http://whotargets.me'><img src='img/wtm.svg' class='portfolio_image' ></a>
+      <p><a href='https://whotargets.me/'>Who Targets Me</a> is a browser extension that collects data about the political adverts people see on Facebook. It was developed in the run up to the 2017 general election to combat 'dark adverts'.</p> 
+
+      <p> It was supported by <a href='https://www.thebureauinvestigates.com/'>The Bureau of Investigative Journalism</a>.</p>
+            
+      <p>I helped the team with communications, writing blog posts and appearing on Radio 4 on two occasions to described the project.</p>
+
+      <br style='clear:both' />
+      <hr />
+
+      <!-- *************************** -->
+
+      <a href='https://xkcd.com/770/'><img src='img/xkcd.png' class='portfolio_image' ></a>
+      <p>The RCA architecture department wanted a method to match students and tutors based on polls of both groups’ preferences. I implemented the Gale-Shapley algorithm, which achieves an optimal matching. Image from <a href='https://xkcd.com/770/'>XKCD</a>. 
+      </p>
+      <br style='clear:both' />
+      <hr />
+
+      <!-- *************************** -->
+      
+      <a href='https://edgeryders.eu/t/mapping-the-opencare-ecosystem-using-twitter/5604/6'><img src='img/opencare.png' class='portfolio_image' ></a>
+      <p><a href='https://edgeryders.eu/t/mapping-the-opencare-ecosystem-using-twitter/5604/6'>OpenCare</a> investigated institutions that provide healthcare outside of the public sector. LocalNets mapped Twitter activity around projects developing open source insulin pumps, 3D printed prosthetics &amp; smartphone based healthcare in developing countries. </p><p> It contributed to Edgeryders report on open healthcare innovation, <a href='https://edgeryders.eu/c/opencare'>commissioned by the EU</a>. </p>
+     
+      <br style='clear:both' />
+      <hr />
+
+      <!-- *************************** -->
+      
+      <a href='http://jimmytidey.co.uk/blog/storymap-participatory-mapping/'><img src='img/sins.png' class='portfolio_image' ></a>
+      <p><a href='http://jimmytidey.co.uk/blog/storymap-participatory-mapping/'>StoryMap</a> was a digtial/physical installation developed as part of the Shakespear in Shoreditch festival. The audeince used typewriters to record annecdotes from around Shoreitch and locate them on a semi-fictional map of east London. It featured local Twitter activity drawn from LocalNets.
+      </p>
+     
+      <br style='clear:both' />
+      <hr />      
+
+      <!-- *************************** -->
+      
+      <a href='https://www.thersa.org/discover/publications-and-articles/reports/community-capital-the-value-of-connected-communities'><img src='img/rsa.jpg' class='portfolio_image' ></a>
+      <p><a href='https://www.thersa.org/discover/publications-and-articles/reports/community-capital-the-value-of-connected-communities'>Connected Communities report with the RSA</a>. The LocalNets Twitter analytics tool was used to map communities in Peterborough and Hounslow. The data was combined with detailed ethnographic surveys carried out the RSA to contribute to their Connected Communities report.  
+      </p>
+     
+      <br style='clear:both' />
+      <hr />            
+
+
+
+      <!-- *************************** -->
       
       <a href='http://wonkbook.io'><img src='img/wonkbook.png' class='portfolio_image' ></a>
       <p><a href='http://wonkbook.io'>Wookbook</a> uses Twitter to evaluate the networks that form between think tanks, politicians and journalists. Every week it creates a league table of the most impactful tweeters, and shows how ideas are spreading across the network.</p>
@@ -45,6 +98,8 @@ include('header.php');
       <br style='clear:both' />
       <hr/>      
       
+
+      <!-- *************************** -->
       
       <a href='http://arthackathon.co.uk'><img src='img/arthack.png' class='portfolio_image' ></a>
       <p><a href='http://arthackathon.co.uk'>Arthackathon</a> was an event sponsored by Ravensbourne University. It brought over 50 artists and coders together to create exhibitable artworks over a weekend. The Localnets tool was used to evaluate changes in the participants social networks.</p>
@@ -52,11 +107,15 @@ include('header.php');
       <br style='clear:both' />
       <hr/>
       
+      <!-- *************************** -->
+
       <img src='img/acrossrca.png' class='portfolio_image' >
       <p>I helped run Metalonders, a week long, cross disciplinary workshop for students joining the RCA. We investigated narratives and social media in four sites across London, producing alternative maps of each location.</p>
       
       <br style='clear:both' />
       <hr/>
+
+      <!-- *************************** -->
       
       <a href='http://www.bbc.co.uk/labuk/results/braintestbritain/'><img src='img/braintrain.png' class='portfolio_image' style='margin-bottom:50px' ></a>
       <p>While at the BBC I worked on <a href='http://www.bbc.co.uk/labuk/results/braintestbritain/'>Brain Test Britain</a>, an online experiment to see if brain training works (it doesn't!) with over 100,000 participants. The work resulted in a <a href='http://www.nature.com/news/2010/100420/full/4641111a.html
@@ -64,6 +123,8 @@ include('header.php');
       
       <br style='clear:both' />
       <hr/>
+
+      <!-- *************************** -->
       
       <a href='http://www.monterosa.co.uk/cases/mpd'><img src='img/mpd.png' class='portfolio_image' ></a>
       <p>I worked on integrating an online game with Channel 4's <a href='http://www.monterosa.co.uk/cases/mpd'>Million Pound Drop TV show</a>. It was one of the most successful integrations of its type, on some occasions over 10% of TV viewers were also playing along online.</p>
