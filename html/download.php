dfebf53067e1dd3cc9dd962c881007700a345d53
Jimmy Tidey <jimmytidey@gmail.com>
2017-10-27 10:59:10 +0100

    checked with gramarly

---
+++ html/download.php
@@ -0,0 +1,9 @@
+<?php
+
+
+$file_url = 'img/jimmy_portfolio_and_cv.zip';
+header('Content-Type: application/octet-stream');
+header("Content-Transfer-Encoding: Binary"); 
+header("Content-disposition: attachment; filename=\"" . basename($file_url) . "\""); 
+readfile($file_url); 
+?>
--- html/index.php
+++ html/index.php
@@ -12,13 +12,13 @@ include('header.php');
 		<div class='col-sm-6 col-sm-offset-1'>
                   <h1>Jimmy Tidey</h1>
       
-            	<p>I'm Jimmy, I'm awaiting my viva for my PhD at The Royal College of Art. In my PhD research I built a Twitter analytics platform for local government, which became a case study for civic social media analytics.</p> 
+            	<p>I'm Jimmy, I'm awaiting my viva for my PhD at The Royal College of Art. My research concerned building and testing a Twitter analytics platform designed for use by local government institutions as a case study in civic uses social media analytics.</p>
 
                   <p>Before starting my PhD I worked on digital products as a developer and a product manager. My undergrad degree is in physics &amp; philosophy.</p> 
                   
-                  <p>My research bringings together my experience of developing digital products with theoretical approaches from network science, economics and public policy.</p>
+                  <p>My research brings together my experience of developing digital products with theoretical approaches from network science, economics, public policy and design research.</p>
 
-                  <p>My blog records <a href='http://jimmytidey.co.uk/blog/category/hack-days/'>hackdays</a>, <a href='http://jimmytidey.co.uk/blog/category/conferences/'>conferences</a>, &amp; <a href='http://jimmytidey.co.uk/blog/lote4-the-stewardship-matera/'>uncategorisable happenings</a> that I've organised or participated in. Some of my projects are presented below.</p>  
+          
             </div>
 
             <div class='col-sm-3 col-sm-offset-1'>
@@ -28,10 +28,12 @@ include('header.php');
                         <li><a href='http://github.com/jimmytidey'>GitHub</a></li>
                         <li><a href='#publications'>Publications &amp; talks</a></li>
                         <li><a href='https://www.linkedin.com/in/jimmytidey'>LinkedIn</a></li>
-                        <li><a href='https://pinboard.in/u:jimmytidey?mode=list&floor=2 '>Bookmarks</a></li>
+                        <li><a href='https://pinboard.in/u:jimmytidey?mode=list&floor=2'>Bookmarks</a></li>
+                        <li><a href='download.php'>CV &amp; Portfolio <span class="glyphicon glyphicon-arrow-down" aria-hidden="true"></span></a></li>
                   </ul>
             </div>              
       </div>
+
 
 
       <div class='row'>
@@ -44,11 +46,16 @@ include('header.php');
                   <a href='http://localnets.org'><img src='img/localnets.png' class='portfolio_image' ></a>
                   
                   <div class='portfolio_text'>
-                        <h4>LocalNets</h4>
-                        <p>LocalNets is a Twitter Analytics tool aimed at local government. I developed LocalNets in the course of my PhD, and it supported all of my field work.</p>
+                        <h4>LocalNets (ongoing)</h4>
+                        <p>Localnets is a Twitter analytics tool designed for use by local government institutions. I developed Localnets in the course of my PhD, using Mongo, Node &amp; the Neo4j graph database.</p>
 
-                        <p>It uses Mongo, Node &amp; Neo4J, and has been piloted with NHS Birmingham South Central Clinical Commissioning Group, <a href='#rsa'>the RSA, NESTA, the Department for Communities and Local Goverment</a>, DemSoc,the <a href='https://gateway-project.org.uk/'>Greenwich Driverless autonomous vehical trial</a> and in numerous other projects.</p> 
+                        <p>It has been piloted with NHS Birmingham Clinical Commissioning Group, <a href='#rsa'>the RSA, the Department for Communities and Local Government</a>, DemSoc, the Greenwich Driverless autonomous vehicle trial and in numerous other projects.</p>
+ 
                   </div>
+
+
+
+
 
                   <br style='clear:both' />
                   <hr />
@@ -57,12 +64,11 @@ include('header.php');
 
                   <a href='http://whotargets.me'><img src='img/wtm.jpg' class='portfolio_image' ></a>
                   <div class='portfolio_text'>
-                        <h4>Who Targets Me</h4>
-                        <p><a href='https://whotargets.me/'>Who Targets Me</a> is a browser extension that collects data about the political adverts people see on Facebook. It was developed in the run up to the 2017 general election to combat 'dark adverts'. It had over 20,000 downloads before the election.</p> 
+                        <h4>Who Targets Me (2017)</h4>
 
-                        <p> It was supported by <a href='https://www.thebureauinvestigates.com/'>The Bureau of Investigative Journalism</a> and The Guardian.</p>
-                              
-                        <p>I helped the team with communications, writing blog posts and appearing on Radio 4 and BBC World Service to described the project.</p>
+                        <p><a href='https://whotargets.me/'>Who Targets Me</a> is a browser extension that collects data about the political adverts people see on Facebook. It was developed in the run up to the 2017 general election to combat ‘dark adverts’. It had over 20,000 installs and has since been deployed in Austria and Germany.</p>
+
+                        <p>It was supported by <a href='https://www.thebureauinvestigates.com/'>The Bureau of Investigative Journalism</a> and <a href='https://www.theguardian.com/technology/2017/may/03/free-software-reveal-facebook-election-posts-targeted-chrome-extension'>The Guardian</a>. I helped the team with communications, writing blog posts and appearing on Radio 4 and BBC World Service to describe the project.</p>
                   </div>
 
                   <br style='clear:both' />
@@ -72,15 +78,16 @@ include('header.php');
 
                   <a href='http://whocites.com'><img src='img/whocites.png' class='portfolio_image' ></a>
                   <div class='portfolio_text'>
-                        <h4>Who Cites</h4>
-                        <p><a href='https://whotcites.com/'>Who Cites</a> is a web app that visualises citation networks to help guide literature reviews. The similar name to Who Targets Me is a coincidence.</p> 
+                        <h4>Who Cites (2016)</h4>
 
-                        <p>Developed to answer a personal need, nearly 300 other academics have used the tool.</p>
+                        <p><a href='https://whotcites.com/'>Who Cites</a> is a web app that visualises citation networks for academic publications using Google Scholar.</p>
+
+                        <p>Developed to answer a personal need, nearly 300 other academics have registered to use the tool.</p>
 
                   </div>
 
                   <br style='clear:both' />
-                  <hr /> 
+                  <hr /> <a name="rsa">
 
                   <!-- *************************** -->
                   
@@ -89,8 +96,8 @@ include('header.php');
                   </a>
                   
                   <div class='portfolio_text'>
-                        <h4>Connected Communities</h4><a name="rsa"></a> 
-                        <p><a href='https://www.thersa.org/discover/publications-and-articles/reports/community-capital-the-value-of-connected-communities'>The Connected Communities report with the RSA</a>. The report drew on two case studies from my PhD research, in Hounslow and Peterborough. The Hounslow work was funded by NESTA, and generated a <a href='https://www.nesta.org.uk/publications/community-mirror-data-driven-method-below-radar-research'>working paper</a>. 
+                        <h4>Connected Communities (2015-2016)</h4></a> 
+                        <p><a href='https://www.thersa.org/discover/publications-and-articles/reports/community-capital-the-value-of-connected-communities'>The Connected Communities report with the RSA</a>. The report drew on two case studies from my PhD research, in Hounslow and Peterborough. The Hounslow work was funded by NESTA and generated a <a href='https://www.nesta.org.uk/publications/community-mirror-data-driven-method-below-radar-research'>working paper</a>. 
                         </p>
 
                         <p>Both projects were based on a collaboration between the RSA, the University of Central Lancashire and the Department of Community and Local Government.
@@ -110,7 +117,7 @@ include('header.php');
                         </a>
                   
                   <div class='portfolio_text'>
-                        <h4>Gale-Shapley algorithm</h4>
+                        <h4>Gale-Shapley algorithm (2017)</h4>
                         <p>The RCA architecture department wanted a method to match students with tutors based on both groups voting on each other. I implemented the Gale-Shapley algorithm, which achieves an optimal matching (for a particular definition of optimal...). Image from <a href='https://xkcd.com/770/'>XKCD</a>. 
                         </p>
                   </div>
@@ -125,8 +132,11 @@ include('header.php');
                   </a>
                   
                   <div class='portfolio_text'> 
-                        <h4>OpenCare</h4>
-                        <p><a href='https://edgeryders.eu/t/mapping-the-opencare-ecosystem-using-twitter/5604/6'>OpenCare</a> investigated institutions that provide healthcare outside of the public sector. LocalNets mapped Twitter activity around projects developing open source insulin pumps, 3D printed prosthetics &amp; smartphone based healthcare in developing countries. </p><p> It contributed to Edgeryders' report on open healthcare innovation, <a href='https://edgeryders.eu/c/opencare'>commissioned by the EU</a>. </p>
+                        <h4> EU OpenCare report (2016)</h4>
+
+                        <p><a href='https://edgeryders.eu/c/opencare'>The OpenCare report</a> investigated non-traditional healthcare projects. Localnets <a href='https://edgeryders.eu/t/mapping-the-opencare-ecosystem-using-twitter/5604/6'>mapped Twitter activity</a> around projects developing open source insulin pumps, 3D printed prosthetics &amp; smartphone based healthcare in developing countries.
+
+                        <p>It contributed to Edgeryders’ (a ‘distributed think tank’) report on open healthcare innovation, commissioned by the EU.</p>
                   </div>     
                   <br style='clear:both' />
                   <hr />
@@ -138,9 +148,9 @@ include('header.php');
                   </a>
 
                   <div class='portfolio_text'>
-                        <h4>StoryMap</h4>
-                        <p><a href='http://jimmytidey.co.uk/blog/storymap-participatory-mapping/'>StoryMap</a> was a digtial/physical installation developed as part of the Shakespear in Shoreditch festival. The audeince used typewriters to record annecdotes from around Shoreitch and locate them on a semi-fictional map of east London. It featured local Twitter activity drawn from LocalNets.
-                        </p>
+                        <h4>StoryMap (2016)</h4>
+
+                        <p><a href='http://jimmytidey.co.uk/blog/storymap-participatory-mapping/'>StoryMap</a> was a digital/physical installation developed as part of the Shakespeare in Shoreditch festival. Participants used typewriters to record anecdotes from around Shoreditch and locate them on a semi-fictional map of east London. It featured local Twitter activity drawn from Localnets. This project was lead by fellow PhD candidate Benjamin Koslowski. </p>
                   </div>      
                  
                   <br style='clear:both' />
@@ -156,7 +166,7 @@ include('header.php');
                   
                   <div class='portfolio_text'>
                         <h4>WonkBook</h4>
-                        <p><a href='http://wonkbook.io'>Wonkbook</a> uses Twitter to evaluate the networks that form between think tanks, politicians and journalists. Every week it creates a league table of the most impactful tweeters, and shows how ideas are spreading across the network.</p>
+                        <p><a href='http://wonkbook.io'>Wonkbook</a> uses Twitter to evaluate the networks that form between think tanks, politicians and journalists. Every week it creates a league table of the most impactful tweeters. It was created in conjunction with Stian Westlake, at the time director of research at NESTA. </p>
                   </div>
                   <br style='clear:both' />
                   <hr/>      
@@ -169,7 +179,8 @@ include('header.php');
                   
                   <div class='portfolio_text'>
                         <h4>Arthackathon</h4>
-                        <p><a href='http://arthackathon.co.uk'>Arthackathon</a> was an event sponsored by Ravensbourne University. It brought over 50 artists and coders together to create exhibitable artworks over a weekend. The Localnets tool was used to evaluate changes in the participants social networks.</p>
+
+                        <p><a href='http://arthackathon.co.uk'>Arthackathon</a> was a hackathon sponsored by Ravensbourne University. It brought over 50 artists and coders together to create exhibitable artworks over a weekend. The Localnets tool was used to evaluate changes in the participant's social networks over the course of the weekend.</p>
                   </div>
 
                   <br style='clear:both' />
@@ -196,9 +207,9 @@ include('header.php');
                   <div class='portfolio_text'>
                         <h4>BBC Lab UK</h4>
                         <p>While at the BBC I worked on <a href='http://www.bbc.co.uk/labuk/results/braintestbritain/'>Brain Test Britain</a>, an online experiment to see if brain training works (it doesn't!) with over 100,000 participants. The work resulted in a <a href='http://www.nature.com/news/2010/100420/full/4641111a.html
-                        '>paper in the journal Nature</a>, and was one of the first psychology experiments carried out on the web.</p>
+                        '>paper in the journal Nature</a> and was one of the first psychology experiments carried out on the web.</p>
 
-                        <p>I also worked on the Great British Class Survey, which generated unique insights into emerging class stuctures in the UK.</p>
+                        <p>I also worked on the Great British Class Survey, which generated unique insights into emerging class structures in the UK.</p>
                   </div>      
                   
                   <br style='clear:both' />
@@ -223,6 +234,14 @@ include('header.php');
                   <hr class='bit_higher' />
                   
                   <div >
+
+                        <p>   
+                              OpenDemocracy, October 2017 -
+                              <a href='https://www.opendemocracy.net/digitaliberties/jimmy-tidey/what-china-can-teach-west-about-digital-democracy'>
+                                    What China can teach the west about digital democracy
+                              </a>
+                        </p>
+
 
                         <p>   
                               HackerNoon, April 2017 -
@@ -325,7 +344,7 @@ include('header.php');
                               </a>
                         </p>
                         <p> 
-                              Aug 2014, Bejing summer school on Cultural Producation -
+                              Aug 2014, Bejing summer school on Cultural Production -
                               <a href='http://jimmytidey.co.uk/blog/cultural-production-age-big-data/'>
                                     What does House of Cards tell us about Big Data and Culture?
                               </a>
